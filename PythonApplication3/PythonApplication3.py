c7bf04d125b4edadf03369eca6d1ecb110e3c267
dcha <dmytro.chasovskykh@gmail.com>
2017-12-02 21:26:31 -0500

    FizzBall

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -1,6 +1,26 @@
 import numpy as np     # installed with matplotlib
 import matplotlib.pyplot as plt
-from math import radians
+from math import radians, modf
+
+def FizzBall():
+# Output numbers from 1 to x. 
+# If the number is divisible by 3, replace it with “Fizz”. 
+# If it is divisible by 5, replace it with “Buzz”. 
+# If it is divisible by 3 and 5 replace it with “FizzBuzz”. '''
+    print("Enter the number please: ");
+    number = int(input());
+    for i in range(1,number+1):
+        tup1 = modf(i/3);
+        tup2 = modf(i/5);
+        if (tup1[0] == 0 and tup2[0] == 0):
+            print("FizzBuzz");
+        elif (tup1[0] == 0 and tup2[0] != 0):
+            print("Fizz");
+        elif (tup1[0] != 0 and tup2[0] == 0):
+            print("Buzz");
+        else:
+            print(i);
+
 
 def main():  
     x = np.arange(0, radians(1800), radians(12))
@@ -9,4 +29,5 @@ def main():
 
 
 
-main()
+#main()
+FizzBall()
